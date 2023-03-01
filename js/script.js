e1289e4eccc42e478e20d8250515becb83d94bd0
Jeremias Toranzo <55718356+jereet@users.noreply.github.com>
2023-02-28 23:58:47 -0300

    Proyecto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("ubicacion").addEventListener("click", function(){
+    document.getElementById("mostrarUb").innerHTML = "Avenida Miller 1235";
+})
+document.getElementById("telefono").addEventListener("click", function(){
+    document.getElementById("mostrarTel").innerHTML = "(935) 838-6345";
+})
+document.getElementById("mail").addEventListener("click", function(){
+    document.getElementById("mostrarMail").innerHTML = "steven.arnold@example.com";
+})
+document.getElementById("trabajo").addEventListener("click", function(){
+    document.getElementById("mostrarTrab").innerHTML = "SAMSUNG";
+})
