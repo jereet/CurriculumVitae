81ff7930f73d42aabebcf15bb26abdfdbc72bb66
Jeremias Toranzo <55718356+jereet@users.noreply.github.com>
2023-03-01 00:16:05 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
+var ubicacion = false;
+var telefono = false; 
+var mail = false;
+var trabajo = false;
 document.getElementById("ubicacion").addEventListener("click", function(){
-    document.getElementById("mostrarUb").innerHTML = "Avenida Miller 1235";
+    if(!ubicacion){
+        document.getElementById("mostrarUb").innerHTML = "Avenida Miller 1235";
+        ubicacion = true;
+    }else{
+        document.getElementById("mostrarUb").innerHTML = "";
+        ubicacion = false;
+    }
+
 })
 document.getElementById("telefono").addEventListener("click", function(){
-    document.getElementById("mostrarTel").innerHTML = "(935) 838-6345";
+    if(!telefono){
+        document.getElementById("mostrarTel").innerHTML = "(935) 838-6345";
+        telefono=true;
+    }else{
+        document.getElementById("mostrarTel").innerHTML = "";
+        telefono = false;
+    }
 })
 document.getElementById("mail").addEventListener("click", function(){
-    document.getElementById("mostrarMail").innerHTML = "steven.arnold@example.com";
+    if(!mail){
+        document.getElementById("mostrarMail").innerHTML = "steven.arnold@example.com";
+        mail=true;
+    }else{
+        document.getElementById("mostrarMail").innerHTML = "";
+        mail = false;
+    }
+
 })
 document.getElementById("trabajo").addEventListener("click", function(){
-    document.getElementById("mostrarTrab").innerHTML = "SAMSUNG";
+    if(!trabajo){
+        document.getElementById("mostrarTrab").innerHTML = "SAMSUNG";
+        trabajo=true;
+    }else{
+        document.getElementById("mostrarTrab").innerHTML = "";
+        trabajo = false;
+    }
 })
